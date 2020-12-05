b733ea9c1b2ca1e80840f91f80f96ac1056af5ed
Alex MacArthur <alex@macarthur.me>
2020-12-04 22:51:17 -0600

    wip - home page art

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head';
 import Meta from '../components/meta'
 import Nav from '../components/Nav';
 
-const clipCover = '50%';
+const clipCover = '40';
 
 export default function Index() {
   return (
@@ -19,24 +19,29 @@ export default function Index() {
         <main className="h-screen w-screen flex items-center justify-center p-6">
           <div className="text-white">
             <h1 className="font-semibold leading-none text-7xl relative text-gray-900">
-              <span className="relative z-20 text-white" style={{
-                '-webkit-text-stroke': '5px white',
+
+              <span className="absolute top-0 left-0 text-white z-10" style={{
+                '-webkit-text-stroke': '5px #a0aec0',
               }}>
                 Alex MacArthur
               </span>
 
-              <span className="absolute top-0 left-0 z-50 text-gray-900" style={{
-                'clip-path': `inset(-1% -1% ${clipCover} -1%)`,
+              <span className="absolute top-0 left-0 text-white z-20" style={{
+              }}>
+                Alex MacArthur
+              </span>
+
+              <span className="relative z-30 text-white" style={{
+                '-webkit-text-stroke': '5px white',
+                'clip-path': `inset(-1% -1% ${clipCover}% -1%)`,
+              }}>
+                Alex MacArthur
+              </span>
+
+              <span className="absolute top-0 left-0 z-40 text-gray-900" style={{
+                'clip-path': `inset(-1% -1% ${clipCover - 5}% -1%)`,
               }}>Alex MacArthur</span>
 
-              <span className="absolute top-0 left-0 z-10 text-white">
-                Alex MacArthur
-              </span>
-
-              {/* <span className="absolute top-0 left-0 z-10 text-white" style={{
-              }}>
-                Alex MacArthur
-              </span> */}
 
             </h1>
             <span className="pl-2 md:pl-0 text-2xl md:text-4xl leading-10 inline-block mt-1 text-gray-500">is a web developer in Nashville-ish, TN.</span>
