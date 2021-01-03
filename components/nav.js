ab4d394a9b66f55eac2c9336ce30bdae36de3729
Alex MacArthur <alex@macarthur.me>
2021-01-02 21:32:30 -0600

    got JS-less menu basically working with some kinks.

--- components/nav.js
+++ components/nav.js
@@ -37,26 +37,62 @@ const Nav = ({classes = ''}) => {
   }, []);
 
   return (
-    <nav className={`py-5 px-5 md:px-8 w-full font-bold flex items-center justify-between ${classes}`}>
+    <nav className={`relative z-10 py-5 px-4 md:px-8 w-full font-bold flex items-center justify-between ${classes}`}>
 
-      <span className={`transition-opacity font-bold text-3xl ${shouldHideLogo ? 'opacity-0' : ''}`}>
+      <span className={`flex-none transition-opacity font-bold text-3xl ${shouldHideLogo ? 'opacity-0' : ''}`}>
         <Logo strokeWidth="2" asLink={true} />
       </span>
 
-      <ul className="flex space-x-4 justify-end">
-        {navItems.map(item => {
-          return (
-            <li
-              className="text-xl font-thin text-gray-500 hover:text-gray-900"
-              key={item.link}
-            >
-              <Link href={item.link}>
-                {item.name}
-              </Link>
-            </li>
-          )
-        })}
-      </ul>
+      <label
+        className="cursor-pointer relative z-20 lg:hidden"
+        htmlFor="menuToggle">
+        Toggle Menu
+      </label>
+
+      <div className="menu-toggler fixed w-full left-0 top-0 h-0 lg:relative lg:h-auto lg:l-0">
+        <input type="checkbox" id="menuToggle" className="relative z-0 opacity-0 lg:hidden" />
+
+        <div className="
+          menu-toggler-items
+          top-0
+          absolute
+          lg:relative
+          transition-all
+          transform
+          translate-x-full
+          opacity-0
+          p-10
+          flex
+          flex-col
+          justify-center
+          bg-white
+          h-screen
+          w-screen
+          lg:relative
+          lg:top-auto
+          lg:translate-x-0
+          lg:opacity-100
+          lg:bg-none
+          lg:h-auto
+          lg:w-auto
+          lg:block
+        ">
+          <ul className="flex flex-col lg:flex-row space-y-4 lg:space-x-4 lg:space-y-0 justify-end">
+            {navItems.map(item => {
+              return (
+                <li
+                className="text-4xl lg:text-xl font-light lg:text-gray-500 hover:text-gray-900 lg:font-200"
+                key={item.link}
+                >
+                  <Link href={item.link}>
+                    {item.name}
+                  </Link>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+      </div>
     </nav>
   )
 }
