2f142e3d5352fb0aa4d48751620eec71656186a3
Acampbell <78562675+fu4303@users.noreply.github.com>
2021-05-01 21:15:33 -0400

    Update [slug].tsx

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -3,37 +3,8 @@ import { getContentBySlug, getAllPosts } from "../../lib/api";
 
 import "prismjs/themes/prism-okaidia.css";
 
-export default function Post({ post, comments, jamCommentsDomain, jamCommentsApiKey }) {
-  return <MarkdownLayout
-    pageData={post}
-    comments={comments}
-    jamCommentsApiKey={jamCommentsApiKey}
-    jamCommentsDomain={jamCommentsDomain}
-    isPost={true}
-  />;
-}
 
-export async function getStaticProps({ params }) {
-  const post = getContentBySlug(params.slug, 'post');
-  const { fetchByPath } = require("@jam-comments/next");
-
-  const comments = await fetchByPath({
-    domain: process.env.JAM_COMMENTS_DOMAIN,
-    apiKey: process.env.JAM_COMMENTS_API_KEY,
-    path: `/posts/${params.slug}`
-  });
-
-  return {
-    props: {
-      jamCommentsApiKey: process.env.JAM_COMMENTS_API_KEY,
-      jamCommentsDomain: process.env.JAM_COMMENTS_DOMAIN,
-      comments,
-      post
-    },
-  };
-}
-
-export async function getStaticPaths() {
+export default function getStaticPaths() {
   const posts = getAllPosts();
 
   return {
